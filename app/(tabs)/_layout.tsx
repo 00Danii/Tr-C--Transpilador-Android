6acbcec0390b968b26ac5eb86c6e2d55d5503f15
00Danii <jdaniel.om97@gmail.com>
2025-11-02 00:14:43 -0600

    sin modal innecesario y archivos de pestañas eliminados

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,9 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 import HomeScreen from '.';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    // <Tabs
-    //   screenOptions={{
-    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-    //     headerShown: false,
-    //     tabBarButton: HapticTab,
-    //   }}>
-    //   <Tabs.Screen
-    //     name="index"
-    //     options={{
-    //       title: 'Transpilador',
-    //       tabBarIcon: ({ color }) => <IconSymbol size={28} name="chevron.left.forwardslash.chevron.right" color={color} />,
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="explore"
-    //     options={{
-    //       title: 'Explorar',
-    //       tabBarIcon: ({ color }) => <IconSymbol size={28} name="lightbulb.fill" color={color} />,
-    //     }}
-    //   />
-    // </Tabs>
     <HomeScreen />
   );
 }
